d7cef6e09c08c6e0dc571c07d7965ecd9bd0f503
andredavila10 <andrealmeidadavila@gmail.com>
2020-08-08 20:10:21 -0300

    Create ExData-Plot.R

---
+++ ExData-Plot.R
@@ -0,0 +1,9 @@
+#Read data
+power <- read.table("household_power_consumption.txt", skip=1, sep=";")
+#Name variables with given labels
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+#Subset power for the two days requested
+powerdates <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007") 
+powerdates$Date <- as.Date(powerdates$Date, format="%d/%m/%Y")
+#Plotting histogram
+hist(as.numeric(as.character(powerdates$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
